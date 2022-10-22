c3bdfc3b3567c15e78c8883c3bc5668808ccc5e4
Vanessa Ragan <azurewings@csu.fullerton.edu>
2022-10-21 21:39:36 -0700

    Update points.py
    Pacman can now eat the points, and they disappear when he eats them. Need to fix their hitbox though

--- points.py
+++ points.py
@@ -1,38 +1,52 @@
 import pygame as pg 
 import csv, os
-from pygame.sprite import Sprite
+from pygame.sprite import Sprite, Group
 from spritesheet import Spritesheet
 from pacman import Pacman
 
 class PointTile(Sprite):
-    def __init__(self, image, x, y, type): #
+    def __init__(self, image, x, y, type):
+        super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
         self.type = type
+        self.eaten = False
     
     def draw(self, surface):
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+        if self.type != 0:
+            surface.blit(self.image, (self.rect.x, self.rect.y))
+            
+    def get_type(self):
+        return self.type
         
     def collected(self):
         print("nom nom")
+        self.eaten = True
+        
+    def update(self):
+        if self.eaten != True:
+            self.draw()
         
 class Points():
     def __init__(self, filename, game):
         self.tile_size = 32
         self.start_x, self.start_y = 0, 0
         self.reg_points = self.load_reg_points(filename)
+        self.all_points = Group()
         self.map_surface = pg.Surface((self.map_w, self.map_h))
         self.map_surface.set_colorkey((0, 0, 0))
         self.pacman = game.pacman 
         self.load_map()
               
     def draw(self, screen):
-        screen.blit(self.map_surface, (0,0))
+        for point in self.all_points.sprites():
+            point.draw(screen)
         
     def load_map(self):
         for reg in self.reg_points:
             reg.draw(self.map_surface)
+            self.all_points.add(reg)
             
     def read_csv(self, filename):
         map = []
@@ -57,10 +71,12 @@ class Points():
         return reg_points
     
     def check_collect(self):
-        col = pg.sprite.spritecollide(self.pacman, self.reg_points, False)
+        col = pg.sprite.spritecollide(self.pacman, self.all_points, False)
         if col:
             for point in col:
                 point.collected()
+                if point.kill():
+                    print()
         
     def update(self):
         self.check_collect()
